b16622aa7a0ac96f1d72e62e0f5c4ee9ab3725cb
DylanTech <>
2025-12-01 20:02:47 +0900

    day 1

--- 2025/solns/day1.py
+++ 2025/solns/day1.py
@@ -1,14 +1,62 @@
 import sys
 
-
 def load_input():
     day = sys.argv[0].replace('solns/day','')[:-3]
     with open(f'solns/{day}.in', 'r') as infile:
         data = [line.rstrip() for line in infile.readlines()]
     return data
 
+#------------------------------------------------
 
-data = load_input()
-print(data)
+def parse(data):
+    return [(line[0], int(line[1:])) for line in data]
+
+#---- Part 1 ------------------------------------
+
+def part1(data):
+    count = 0
+    insts = parse(data)
+
+    pos = 50
+    for dir, val in insts:
+        pos += val if dir == 'R' else -1*val
+        pos %= 100
+
+        if pos == 0:
+            count += 1
+
+    return count
+
+
+#---- Part 1 ------------------------------------
+
+def part2(data):
+    count = 0
+    insts = parse(data)
+
+    pos = 50
+    for dir, val in insts:
+        for _ in range(val):
+            if dir == 'R':
+                pos += 1
+            else:
+                pos -= 1
+
+            if pos == -1 or pos == 100:
+                pos %= 100
+            
+            if pos == 0:
+                count += 1
+
+    return count
+
+
+#================================================
+#       MAIN
+#================================================
+if __name__=='__main__':
+    data = load_input()
+    print(f'Part 1:', part1(data))
+    print(f'Part 2:', part2(data))
 
 
--- 2025/solns/template.py
+++ 2025/solns/template.py
@@ -1,14 +1,34 @@
 import sys
 
-
 def load_input():
     day = sys.argv[0].replace('solns/day','')[:-3]
     with open(f'solns/{day}.in', 'r') as infile:
         data = [line.rstrip() for line in infile.readlines()]
     return data
 
+#------------------------------------------------
 
-data = load_input()
-print(data)
+def parse(data):
+    pass
+
+#---- Part 1 ------------------------------------
+
+def part1():
+    pass
+
+
+#---- Part 1 ------------------------------------
+
+def part2():
+    pass
+
+
+#================================================
+#       MAIN
+#================================================
+if __name__=='__main__':
+    data = load_input()
+    print(f'Part 1:', part1(data))
+    print(f'Part 2:', part2(data))
 
 
